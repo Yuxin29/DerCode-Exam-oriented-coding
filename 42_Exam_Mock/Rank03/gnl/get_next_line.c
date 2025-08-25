5f6c27c6768ef2b54c048a68692966f1f9339b85
Yuxin29 <yuxin.1.wu@gmail.com>
2025-08-26 00:12:34 +0300

    preparing

--- 42_Exam_Mock/Rank03/gnl/get_next_line.c
+++ 42_Exam_Mock/Rank03/gnl/get_next_line.c
@@ -1,13 +1,153 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yuwu <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/25 18:53:48 by yuwu              #+#    #+#             */
-/*   Updated: 2025/08/25 18:53:57 by yuwu             ###   ########.fr       */
-/*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.g"
 
+static int ft_strlen(char *s)
+{
+    int i;
+
+    i = 0;
+    if (!*s)
+        return (i);
+    while (s[i])
+        i++;
+    return (i);
+}
+
+statich char *ft_strchr(char *s, char c)
+{
+    if (!*s)
+        return (NULL);
+    while (*s)
+    {
+        if (*s == c)
+            return ((char *)s);
+        s++;
+    }
+    if (c == '\0')
+        return (*s);
+    return (NULL);
+}
+
+static char *ft_strjoin(char *s1, char *s2)
+{
+    int     len1;
+    int     len2;
+    int     i;
+    char    *joined;
+    
+    i = 0;
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    joined = malloc(len1 + len2 + 1);
+    if (!joined)
+        return (NULL);
+    while (i < len1)
+    {
+        joined[i] = s1[i];
+        i++;
+    }
+    i = 0;
+    while (i < len2)
+    {
+        joined[len1 + i] = s2[i];
+        i++;
+    }
+    joined[len1 + len2 + 1] = '\0';
+    return (joined);
+}
+
+static char *extract_and_update(char **stash)
+{
+	int	    i;
+    int     j;
+	char	*line;
+	char	*newstash;
+
+    i = 0;
+    j = 0;
+	if (!*stash || !(*stash)[0])
+		return (NULL);
+
+	while ((*stash)[i] && (*stash)[i] != '\n')
+		i++;
+	if ((*stash)[i] == '\n')
+		i++;
+
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	while (j < i)
+	{
+		line[j] = (*stash)[j];
+		j++;
+	}
+	line[j] = '\0';
+
+	if (!(*stash)[i])
+	{
+		free(*stash);
+		*stash = NULL;
+		return (line);
+	}
+	newstash = malloc(ft_strlen(*stash) - i + 1);
+	if (!newstash)
+		return (NULL);
+	j = 0;
+	while ((*stash)[i])
+		newstash[j++] = (*stash)[i++];
+	newstash[j] = '\0';
+
+	free(*stash);
+	*stash = newstash;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char    *stash;
+	char	buffer[BUFFER_SIZE + 1];
+	int		bytes_read;
+	char	*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(stash, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(stash);
+			stash = NULL;
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		if (bytes_read > 0)
+			stash = ft_strjoin(stash, buffer);
+	}
+	line = extract_and_update(&stash);
+	return (line);
+}
+
+int main(int argc, char **argv)
+{
+    int     fd;
+    char    *line;
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+    {
+        perror("open");
+        return (1);
+    }
+    printf("ft=0\n");
+    while ((line = get_next_line(fd)) != NULL) // fd = 0 = stdin
+    {
+        printf("%s", line);
+        free(line);
+    }
+    return (0);
+}
--- 42_Exam_Mock/Rank03/printf/ft_printf.c
+++ 42_Exam_Mock/Rank03/printf/ft_printf.c
@@ -1,62 +1,115 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yuwu <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/25 18:47:21 by yuwu              #+#    #+#             */
-/*   Updated: 2025/08/25 18:53:12 by yuwu             ###   ########.fr       */
-/*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h> //malloc, free
 #include <unistd.h>
-//va_start, va_arg, va_copy, va_end
-
-//s,d, and x
+#include <stdarg.h> //va_start, va_arg, va_copy, va_end
 
 //Before you start we advise you to read the `man 3 printf` and the `man va_arg`.
 //To test your program compare your results with the true printf.
 
+//s,d, and x
 static int ft_putstr(char *str)
 {
-	int	i;
+	int		i;
+	char	c;
 
 	i = 0;
+	if (!str)
+		return (write(1, "(null)", 1));
 	while (str[i])
 	{
-		if write(1, str[i], 1) = -1;
+		c = str[i];
+		if (write(1, &c, 1) == -1);
 			return (-1);
 		i++;
 	}
 	return (i);
 }
 
-static int ft_putdec(char *str)
+static int ft_putdec(int n)
 {
-	//
+	int 	count;
+	int		rest;
+	char	c;
+
+	if (n < 0)
+	{
+		n = -n;
+		count = write(1, "-", 1);
+		if (count == -1)
+			return (-1);
+	}
+	if (n >= 10)
+	{
+		rest = ft_putdec(n / 10);
+		if (rest == -1)
+			return (-1);
+		count += rest;
+	}
+	c = (n % 10) - '0';
+	if (write(1, &c, 1) == -1)
+		return (-1);
+	return (count);
+}
+
+static int ft_puthex(unsigned int n)
+{
+	int	i;
+
+	i = 0;
 	return (i);
 }
 
-static int ft_puthex(char *str)
+int ft_printf(const char *str, ... )
 {
-	//
-	return (i);
+	va_list args;
+	int		result;
+	int		i;
+	int		count;
+
+	va_start(args,str);
+	i = 0;
+	count = 0;
+	if (!str)
+		return(-1);
+	while (str[i])
+	{
+		if (str[i] == '%' && str[i + 1])
+		{
+			i++;
+			if (str[i] == 's')
+				result = ft_putstr(va_arg(args, char *));
+			else if (str[i] == 'd')
+				result = ft_putdec(va_arg(args, int));
+			else if (str[i] == 'x')
+				result = ft_puthex(va_arg(args, unsigned int));
+			else
+				result = write(1, &str[i], 1);
+		}
+		else 
+			result = write(1, &str[i], 1);
+		if (result == -1)
+			return (-1);
+		count += result;
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
 
-int ft_printf(const char *, ... );
+// call: ft_printf("Hello %s\n", "toto");
+// out:Hello toto$
+
+// call: ft_printf("Magic %s is %d", "number", 42);
+// out:Magic number is 42%
+
+// call: ft_printf("Hexadecimal for %d is %x\n", 42, 42);
+// out:Hexadecimal for 42 is 2a$
+
+int main(void)
 {
-
-
+	ft_printf("Hello %s\n", "toto");
+	return (0);
 }
-
-
-call: ft_printf("Hello %s\n", "toto");
-out:Hello toto$
-
-call: ft_printf("Magic %s is %d", "number", 42);
-out:Magic number is 42%
-
-call: ft_printf("Hexadecimal for %d is %x\n", 42, 42);
-out:Hexadecimal for 42 is 2a$
