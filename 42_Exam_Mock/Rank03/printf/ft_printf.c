00c6409612ec377b7cc44a40a8823b35aa1167e3
Yuxin Wu <yuwu@c3r1p7.hive.fi>
2025-08-26 11:57:31 +0300

    printf shorter

--- 42_Exam_Mock/Rank03/printf/ft_printf.c
+++ 42_Exam_Mock/Rank03/printf/ft_printf.c
@@ -1,93 +1,88 @@
 /* ************************************************************************** */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include <stdio.h> //malloc, free
-#include <unistd.h>
+#include <stdio.h> //printf
+#include <unistd.h> //write
 #include <stdarg.h> //va_start, va_arg, va_copy, va_end
 
-//Before you start we advise you to read the `man 3 printf` and the `man va_arg`.
-//To test your program compare your results with the true printf.
-
-//s,d, and x
 static int ft_putstr(char *str)
 {
 	int	i;
-	char	c;
-
+	
 	i = 0;
 	if (!str)
-		return (write(1, "(null)", 1));
-	while (str[i])
+		return (write(1, "(null)", 6));
+	while(str[i])
 	{
-		c = str[i];
-		if (write(1, &c, 1) == -1)
+		if (write(1, &str[i], 1) == -1)
 			return (-1);
 		i++;
 	}
 	return (i);
 }
 
-static int ft_putdec(int n)
-{
-	int 	count;
-	int	rest;
-	char	c;
-
-	count = 0;
-	if (n < 0)
-	{
-		n = -n;
-		count = write(1, "-", 1);
-		if (count == -1)
-			return (-1);
-	}
-	if (n >= 10)
-	{
-		rest = ft_putdec(n / 10);
-		if (rest == -1)
-			return (-1);
-		count += rest;
-	}
-	c = (n % 10) + '0';
-	if (write(1, &c, 1) == -1)
-		return (-1);
-	count++;
-	return (count);
-}
-
-static int ft_puthex(unsigned int n)
+static int	ft_putdec(int	n)
 {
 	int	count;
-	int	rest;
-	char 	c;
-	
+	int	ret;
+	char	c;
 	count = 0;
-	if (n >= 16)
-	{	
-		rest = ft_puthex(n / 16);
-		if (rest == -1)
+	if (n == -2147483648)
+		return (write(1, "-2147483648", 11));
+	if (n < 0)
+	{
+		if (write(1, "-", 1) == -1)
 			return (-1);
-		count += rest;
+		count = 1;
+		n = -n;
 	}
-	c = "0123456789abcdef"[n % 16];
-	if (write(1, &c, 1) == -1)
+	if (n / 10 > 0)
+	{
+		ret = ft_putdec(n / 10);
+		if (ret == -1)
+			return (-1);
+		count += ret;
+	}
+	c = (n % 10) + '0';
+	ret = write(1, &c, 1);
+	if (ret == -1)
 		return (-1);
-	count++;
+	count += ret;
 	return (count);
 }
 
-int	ft_printf(const char *str, ...)
+static int	ft_puthex(unsigned int	n)
+{
+	int	count;
+	int	ret;
+	char	c;
+	
+	count = 0;
+	if (n / 16 > 0)
+	{
+		ret = ft_puthex(n / 16);
+		if (ret == -1)
+			return (-1);
+		count += ret;
+	}
+	c = "0123456789abcdef"[n % 16];
+	ret = write(1, &c, 1);
+	if (ret == -1)
+		return (-1);
+	count += ret;
+	return (count);
+}
+
+int	ft_printf(char *str, ...)
 {
 	va_list args;
 	int	count;
-	int	ret;
 	int	i;
+	int	ret;
 	
-	var_start(args, str);
 	count = 0;
 	i = 0;
-	if (!str)
+	va_start(args, str);
+	if (!str || !*str)
 		return (-1);
 	while (str[i])
 	{
@@ -95,11 +90,11 @@ int	ft_printf(const char *str, ...)
 		{
 			i++;
 			if (str[i] == 's')
-				ret = return(ft_putstr(va_arg(args, char *)));
-			else if (str[i] = 'd')
-				ret = return(ft_putdec(va_arg(args, int)));
-			else if (str[i] = 'x')
-				ret = return(ft_puthex(va_arg(args, unsigned int)));
+				ret = ft_putstr(va_arg(args, char *));
+			else if (str[i] == 'd')
+				ret = ft_putdec(va_arg(args, int));
+			else if (str[i] == 'x')
+				ret = ft_puthex(va_arg(args, unsigned int));
 			else
 				ret = write(1, &str[i], 1);
 		}
@@ -107,22 +102,20 @@ int	ft_printf(const char *str, ...)
 			ret = write(1, &str[i], 1);
 		if (ret == -1)
 			return (-1);
-		count += ret;
 		i++;
+		count += ret;
 	}
 	va_end(args);
 	return (count);
 }
 
-int main(void)
+int	main(void)
 {
-	printf("Hello %s\n", "atoto");
-	ft_printf("Hello %s\n", "atoto");
-	printf("Hello %d\n", 24534);
-	ft_printf("Hello %d\n", 24534);
-	printf("Hello %d\n", -24534);
-	ft_printf("Hello %d\n", -24534);
-	printf("Hello %x\n", 16);
-	ft_printf("Hello %x\n", 16);
+	ft_printf("%s\n", "te");
+	printf("%s\n", "te");
+	ft_printf("%d\n", -223);
+	printf("%d\n", -223);
+	ft_printf("%x\n", 2223);
+	printf("%x\n", 2223);
 	return (0);
 }
